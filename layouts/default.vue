19c1b1a259c33d58a45638861125a3c705954e63
Jason Suttles <Jason Suttles>
2017-12-02 09:40:27 -0500

    default to closed drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -84,7 +84,7 @@
     data () {
       return {
         clipped: false,
-        drawer: true,
+        drawer: false,
         fixed: false,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
