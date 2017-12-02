e2b2264d625ac5c67ea7a61bfdd97aac427e9f9c
Jason Suttles <Jason Suttles>
2017-12-02 09:15:55 -0500

    initial commit

---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">Login</v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs8>
+                <v-text-field
+                  name="username"
+                  label="Username"
+                  id="testing"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs8>
+                <v-text-field
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="testing"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat>Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
---
+++ plugins/vuetify.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VCard,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  VTextField
+} from 'vuetify'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#9c27b0',
+    accent: '#ce93d8',
+    secondary: '#424242',
+    info: '#0D47A1',
+    warning: '#ffb300',
+    error: '#B71C1C',
+    success: '#2E7D32'
+  },
+  components: {
+    VApp,
+    VCard,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    VTextField
+  }
+})
