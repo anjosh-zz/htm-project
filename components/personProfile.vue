7a8bcc626ee4cafbc0771a4d4565605deed7546a
Jason Suttles <Jason Suttles>
2018-01-06 14:51:11 -0500

    Added an edit button and made all the buttons close the modal

--- components/personProfile.vue
+++ components/personProfile.vue
@@ -1,12 +1,31 @@
 <template>
   <v-layout row justify-center>
-    <v-dialog v-model="dialog" max-width="290">
+    <v-dialog v-model="dialog" max-width="290" transition="slide-y-transition">
       <v-card>
         <v-card-media :src="profilePic" height="200px">
           <v-container fluid class="full-height">
             <v-layout row>
-              <v-flex xs12>
-                <v-icon  v-bind:class="[imageTextColor]">arrow_back</v-icon>
+              <v-flex xs6>
+                <v-btn
+                  flat
+                  icon
+                  color="grey"
+                  class="ma-0"
+                  @click.native="hideProfile"
+                >
+                  <v-icon  v-bind:class="[imageTextColor]">arrow_back</v-icon>
+                </v-btn>
+              </v-flex>
+              <v-flex xs6 class="text-xs-right">
+                <v-btn
+                  flat
+                  icon
+                  color="grey"
+                  class="ma-0"
+                  @click.native="hideProfile"
+                >
+                  <v-icon  v-bind:class="[imageTextColor]"  @click.native="hideProfile">edit</v-icon>
+                </v-btn>
               </v-flex>
             </v-layout>
             <v-layout row>
@@ -22,7 +41,8 @@
           </div>
         </v-card-title>
         <v-card-actions>
-        <v-btn color="green darken-1" flat="flat" @click.native="hideProfile">Agree</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" flat="flat" @click.native="hideProfile">Close</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
