d4c08ed5256b6642d84b667144aae046b614f232
Joshua Angelucci <joshuaangelucci@Joshuas-MacBook-Pro.local>
2019-04-23 16:16:38 -0400

    add auth0 badge

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,7 +38,13 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <v-footer :fixed="fixed" :height="56" app>
+      <a width="150" height="50" href="https://auth0.com/?utm_source=oss&utm_medium=gp&utm_campaign=oss" target="_blank"
+         alt="Single Sign On & Token Based Authentication - Auth0">
+        <img width="150" height="50"
+             alt="JWT Auth for open source projects"
+             src="//cdn.auth0.com/oss/badges/a0-badge-light.png"/>
+      </a>
     </v-footer>
   </v-app>
 </template>
