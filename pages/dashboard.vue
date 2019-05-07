7cbc63170db134af016b940ec1e0acba43feb51b
Joshua Angelucci <joshuaangelucci@Joshuas-MacBook-Pro.local>
2019-05-07 14:38:20 -0400

    add add guest button to Dashboard page

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,74 +1,87 @@
 <template>
   <v-layout row align-center justify-center>
     <v-flex xs12 sm5>
-    <v-card>
-      <v-card-title class="headline pb-0">Dashboard</v-card-title>
-      <v-container
-          fluid
-          grid-list-lg
+      <v-btn
+          fixed
+          dark
+          fab
+          bottom
+          right
+          color="primary"
+          class="mb-5 mr-2"
+          nuxt
+          to="/addguest"
       >
-        <v-layout row wrap>
-          <v-flex>
-            <v-card>
-              <v-card-title primary-title>
-                <v-icon color="blue darken-2">contacts</v-icon>
-              </v-card-title>
-              <v-card-text>
-                <div class="display-2">{{contacts}}</div>
-              </v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions class="pa-3">
-                <div class="subheading">Total Contacts</div>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+        <v-icon>add</v-icon>
+      </v-btn>
+      <v-card>
+        <v-card-title class="headline pb-0">Dashboard</v-card-title>
+        <v-container
+            fluid
+            grid-list-lg
+        >
+          <v-layout row wrap>
+            <v-flex>
+              <v-card>
+                <v-card-title primary-title>
+                  <v-icon color="blue darken-2">contacts</v-icon>
+                </v-card-title>
+                <v-card-text>
+                  <div class="display-2">{{contacts}}</div>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions class="pa-3">
+                  <div class="subheading">Total Contacts</div>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
 
-          <v-flex xs12>
-            <v-card>
-              <v-card-title primary-title>
-                <v-icon color="purple darken-1">fa-wine-glass</v-icon>
-              </v-card-title>
-              <v-card-text>
-                <div class="display-2">{{oneStepCouples}}</div>
-              </v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions class="pa-3">
-                <div class="subheading">Couples given Holy Wine</div>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+            <v-flex xs12>
+              <v-card>
+                <v-card-title primary-title>
+                  <v-icon color="purple darken-1">fa-wine-glass</v-icon>
+                </v-card-title>
+                <v-card-text>
+                  <div class="display-2">{{oneStepCouples}}</div>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions class="pa-3">
+                  <div class="subheading">Couples given Holy Wine</div>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
 
-          <v-flex xs12>
-            <v-card>
-              <v-card-title primary-title>
-                <v-icon color="red darken-1">fa-magic</v-icon>
-              </v-card-title>
-              <v-card-text>
-                <v-layout row align-center>
-                  <v-flex xs2>
-                    <span class="display-2">{{threeStepCouples}}</span>
-                  </v-flex>
-                  <v-flex xs10 v-if="threeStepCouplesProgress > 10">
-                    <v-progress-circular
-                        :width="20"
-                        :size="40"
-                        color="white"
-                        :value="threeStepCouplesProgress">
-                    </v-progress-circular>
-                  </v-flex>
-                </v-layout>
-              </v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions class="pa-3">
-                <div class="subheading">Couples completed Chastening Ceremony</div>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+            <v-flex xs12>
+              <v-card>
+                <v-card-title primary-title>
+                  <v-icon color="red darken-1">fa-magic</v-icon>
+                </v-card-title>
+                <v-card-text>
+                  <v-layout row align-center>
+                    <v-flex xs2>
+                      <span class="display-2">{{threeStepCouples}}</span>
+                    </v-flex>
+                    <v-flex xs10 v-if="threeStepCouplesProgress > 10">
+                      <v-progress-circular
+                          :width="20"
+                          :size="40"
+                          color="white"
+                          :value="threeStepCouplesProgress">
+                      </v-progress-circular>
+                    </v-flex>
+                  </v-layout>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions class="pa-3">
+                  <div class="subheading">Couples completed Chastening Ceremony</div>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
 
-        </v-layout>
-      </v-container>
-    </v-card>
-  </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
